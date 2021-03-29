9659356a0e502f67d8879cb2937af3781e79f3dc
Valentin <valentin.rudloff.perso@gmail.com>
2021-03-29 22:04:47 +0200

    create main python

---
+++ main.py
@@ -0,0 +1,13 @@
+import cv2
+
+def combine_frame_geolocation(frame, location):
+    print()
+
+
+def main():
+    print()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
