522102f5a547e3c0c568938b1e607868e356afc4
valentin <valentin.rudloff@navya.tech>
2021-04-02 20:56:00 +0200

    update proto scripts

--- notebook/extract_proto_photo.py
+++ notebook/extract_proto_photo.py
@@ -18,36 +18,43 @@ def show_map(lat, lon, id):
         img = img.convert(self.desired_tile_form)  # set image format
         return img, self.tileextent(tile), 'lower'  # reformat for cartopy
 
-    cimgt.OSM.get_image = image_spoof  # reformat web request for street map spoofing
-    #osm_img = cimgt.GoogleTiles()  # spoofed, downloaded street map
-    osm_img = cimgt.QuadtreeTiles()  # spoofed, downloaded street map
+    import cartopy.io.img_tiles as cimgt
 
-    fig = plt.figure(figsize=(12, 9))  # open matplotlib figure
-    ax1 = plt.axes(projection=osm_img.crs)  # project using coordinate reference system (CRS) of street map
+
+
+    cimgt.OSM.get_image = image_spoof  # reformat web request for street map spoofing
+    osm_img = cimgt.GoogleTiles()  # spoofed, downloaded street map
+    #osm_img = cimgt.QuadtreeTiles()  # spoofed, downloaded street map
+
+    fig = plt.figure(figsize=(5,5), frameon=False)
+    ax1 = plt.axes(projection=osm_img.crs)
     center_pt = [lat[-1], lon[-1]]  # lat/lon of One World Trade Center in NYC
-    zoom = 0.01  # for zooming out of center point
+    zoom = 0.1  # for zooming out of center point
     print(center_pt)
-    extent = [center_pt[1] - (zoom * 2.0), center_pt[1] + (zoom * 2.0), center_pt[0] - zoom,
+    extent = [center_pt[1] - (zoom * 1.5), center_pt[1] + (zoom * 1.5), center_pt[0] - zoom,
               center_pt[0] + zoom]  # adjust to zoom
+    print(extent)
     ax1.set_extent(extent)  # set extents
 
     scale = np.ceil(-np.sqrt(2) * np.log(np.divide(zoom, 350.0)))  # empirical solve for scale based on zoom
     scale = (scale < 20) and scale or 19  # scale cannot be larger than 19
     ax1.add_image(osm_img, int(scale))  # add OSM with zoom specification
+    ax1.set_axis_off()
+    fig.add_axes(ax1)
     # NOTE: zoom specifications should be selected based on extent:
     # -- 2     = coarse image, select for worldwide or continental scales
     # -- 4-6   = medium coarseness, select for countries and larger states
     # -- 6-10  = medium fineness, select for smaller states, regions, and cities
     # -- 10-12 = fine image, select for city boundaries and zip codes
     # -- 14+   = extremely fine image, select for roads, blocks, buildings
-    plt.plot(lon, lat, color='red', linewidth=2, marker='o', alpha=0.5,
+    plt.plot(lon, lat, color='red', linewidth=2, alpha=0.5,
              transform=ccrs.Geodetic(),
              )
     plt.plot(lon[-1], lat[-1],
-             color='blue', markersize=10, marker='^',
+             color='blue', markersize=10, marker='o',
              transform=ccrs.Geodetic(),
              )
-    plt.savefig("figure"+str(id)+".png")
+    plt.savefig("map/"+str(id)+".png",bbox_inches='tight', pad_inches=0)
     #plt.show()
     plt.close()
 
@@ -61,6 +68,6 @@ lon_list = []
 for lat, lon in zip(lat_lines, lon_lines) :
     lat_list.append(float(lat.strip('\n')))
     lon_list.append(float(lon.strip('\n')))
-    show_map(lat_list, lon_list, len(lat_list))
+    show_map(lat_list, lon_list, len(lat_list)-1)
 lat_file.close()
 lon_file.close()
