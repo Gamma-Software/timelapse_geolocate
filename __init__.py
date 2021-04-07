468bacdb5995dd4b79371cc4eea5e700dc316570
valentin <valentin.rudloff@navya.tech>
2021-04-07 19:46:13 +0200

    change init folders

--- __init__.py
+++
@@ -1,1 +0,0 @@
-name = 'timelapsegeo_pkg'
--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,5 @@
 if __name__ == "__main__":
-    from .timelapsegeo import main
+    from timelapsegeo import main
     import colorama
     colorama.init()
     main()
