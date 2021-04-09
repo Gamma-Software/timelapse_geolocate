8502c88a62bfa180dc1830bb29f9028438cb976d
valentin <valentin.rudloff@navya.tech>
2021-04-10 00:43:45 +0200

    add Timelapse Argument update tests

--- tests/unit/test_generate_timelapse.py
+++ tests/unit/test_generate_timelapse.py
@@ -3,28 +3,45 @@ from unittest import TestCase
 from src.timelapsegeo import generate_timelapse
 
 
+class TimelapseArguments:
+    cached_frames_folder = ""
+    output_filename = ""
+    framerate = 0.0
+
 class TestGenerateTimelapse(TestCase):
     def test_generate_timelapse_no_framerate(self):
+        args = TimelapseArguments()
+        args.cached_frames_folder = "tests/unit/data/test3"
+        args.output_filename = "output.mp4"
+        args.framerate = -1.0
         print("Running test_generate_timelapse_no_framerate")
-        self.assertEqual(generate_timelapse("data/test3", "output.mp4", -1), False)
+        self.assertEqual(generate_timelapse(args), False)
 
     def test_generate_timelapse(self):
         print("Running test_generate_timelapse")
         # Case 1 False: The folder is empty
         print("Running Case 1")
-        self.assertEqual(generate_timelapse("data/test1", "output.mp4", 10), False)
+        args = TimelapseArguments()
+        args.cached_frames_folder = "tests/unit/data/test1"
+        args.output_filename = "output.mp4"
+        args.framerate = 10.0
+        self.assertEqual(generate_timelapse(args), False)
         # Case 2 False: The folder contains only one frame
         print("Running Case 2")
-        self.assertEqual(generate_timelapse("data/test2", "output.mp4", 10), False)
+        args.cached_frames_folder = "tests/unit/data/test2"
+        self.assertEqual(generate_timelapse(args), False)
         # Case 3 False: The folder contains two frames but not the same size
         print("Running Case 3")
-        self.assertEqual(generate_timelapse("data/test3", "output.mp4", 10), False)
+        args.cached_frames_folder = "tests/unit/data/test3"
+        self.assertEqual(generate_timelapse(args), False)
         # Case 4 True: The folder contains two frames the same size
         print("Running Case 4")
-        self.assertEqual(generate_timelapse("data/test4", "output.mp4", 10), True)
+        args.cached_frames_folder = "tests/unit/data/test4"
+        self.assertEqual(generate_timelapse(args), True)
         # Case 5 False: The folder does not exist
         print("Running Case 5")
-        self.assertEqual(generate_timelapse("data/test5", "output.mp4", 10), False)
+        args.cached_frames_folder = "tests/unit/data/test5"
+        self.assertEqual(generate_timelapse(args), False)
 
         # Remove output.mp4 generated if exists
         import os
