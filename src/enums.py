fba48cdf8e957202d318adbb0266220b5f04d747
capsule <valentin.rudloff.perso@gmail.com>
2021-07-04 23:34:13 +0200

    update

--- src/enums.py
+++ src/enums.py
@@ -5,3 +5,8 @@ class TimelapseGeneratorCommand(Enum):
     PAUSE = 0
     RESUME = 1
     STOP = 2
+
+class Motion(Enum):
+    STOP = 0
+    IDLE = 1 # Car is started but still not moving
+    DRIVE = 2
