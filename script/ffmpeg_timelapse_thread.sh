f8c74a78753c049ee3a0d099ef53a5534102514f
capsule <valentin.rudloff.perso@gmail.com>
2021-07-08 14:42:27 +0200

    add shell folder creation and execute ffmpeg timelapse

---
+++ script/ffmpeg_timelapse_thread.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Create the folder
+PICS_FOLDER=$(pwd)
+FOLDER=$(date +"%Y%m%d_%H%M%S")
+mkdir -p $PICS_FOLDER/$FOLDER
+cd $PICS_FOLDER/$FOLDER
+
+# Launch the timelapse and store the images in the pic folder horodated
+ffmpeg -rtsp_transport tcp -y -i "rtsp://admin:pam1249CS1110ragot&@192.168.1.23:554//h264Preview_01_main" -q:v 4 -vf fps=$2 -strftime 1 "%Y-%m-%d_%H-%M-%S.jpg"
