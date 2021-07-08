b3f0500da1a2f2217aed0658634b15d4aa0d7353
capsule <valentin.rudloff.perso@gmail.com>
2021-07-08 14:41:52 +0200

    add folder creation

--- experiments/capt.sh
+++ experiments/capt.sh
@@ -13,5 +13,5 @@ ffmpeg -rtsp_transport tcp -y -i "rtsp://admin:pam1249CS1110ragot&@192.168.1.23:
 ffmpeg -rtsp_transport tcp -y -i "rtsp://admin:pam1249CS1110ragot&@192.168.1.23:554//h264Preview_01_main" -q:v 4 -vf fps=1 -strftime 1 "%Y-%m-%d_%H-%M-%S.jpg"
 
 # Get create folder
-ffmpeg -rtsp_transport tcp -y -i "rtsp://admin:pam1249CS1110ragot&@192.168.1.23:554//h264Preview_01_main" -q:v 4 -vf fps=1 -strftime 1 -strftime_mkdir 1 "%Y-%m-%d_%H-%M-%S.jpg"
+ffmpeg -rtsp_transport tcp -y -i "rtsp://admin:pam1249CS1110ragot&@192.168.1.23:554//h264Preview_01_main" -q:v 4 -vf fps=1 -strftime 1 -strftime_mkdir 1 "%Y%m%d_%H%M%S/%Y-%m-%d_%H-%M-%S.jpg"
 
