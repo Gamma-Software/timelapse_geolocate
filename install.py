e23885cd6d1702dbe2c1cfd7b53d44b67b0a3071
capsule <valentin.rudloff.perso@gmail.com>
2021-08-11 00:02:02 +0000

    start service at install and give write permission to rudloff

--- install.py
+++ install.py
@@ -9,12 +9,11 @@ def create_path(path):
         print("Create folder", path)
         os.makedirs(path, 0o740)
         os.chown(path, 1000, 1000) # Rudloff id and group Root
-        os.chmod(path, 0o740) # Give all read access but Rudloff write access 
+        os.chmod(path, 0o774) # Give all read access but Rudloff write access 
 
 print("Configuring timelapse_trip app...")
 path_to_services = "/etc/systemd/system/timelapse_trip.service"
 create_path(path_to_app)
-create_path(path_to_log)
 create_path(path_to_timelapse_tmp)
 create_path(path_to_current_results)
 if not os.path.exists(path_to_conf):
@@ -28,3 +27,4 @@ if not os.path.exists(path_to_services):
     os.chmod(path_to_services, 0o775) # Give all read access but Rudloff write access
     os.system("systemctl daemon-reload")
     os.system("systemctl enable timelapse_trip.service")
+    os.system("systemctl start timelapse_trip.service")
