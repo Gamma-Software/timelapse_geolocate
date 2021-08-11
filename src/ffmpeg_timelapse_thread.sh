f3b35227b1231c734f79edcd062b2e5b0f194f9d
capsule <valentin.rudloff.perso@gmail.com>
2021-08-11 00:03:51 +0000

    fix default camera config

--- src/ffmpeg_timelapse_thread.sh
+++ src/ffmpeg_timelapse_thread.sh
@@ -6,8 +6,9 @@ FOLDER=$(date +"%Y-%m-%d_%H-%M-%S")
 mkdir -p $PICS_FOLDER/$FOLDER
 cd $PICS_FOLDER/$FOLDER
 
+# TODO refactor to avoid using shell script
 # Launch the timelapse and store the images in the pic folder horodated
-ffmpeg -rtsp_transport tcp -y -i "rtsp://admin:pam1249CS1110ragot&@192.168.1.23:554//h264Preview_01_main" -q:v 4 -vf fps=$1 -strftime 1 "%Y-%m-%d_%H-%M-%S.jpg"
+ffmpeg -rtsp_transport tcp -y -i "rtsp://admin:pam1249CS1110ragot&@camera:554//h264Preview_01_main" -q:v 4 -vf fps=$1 -strftime 1 "%Y-%m-%d_%H-%M-%S.jpg"
 
 # https://ffmpeg.org/ffmpeg.html#Synopsis
 #ffmpeg -y -i "rtsp://admin:pam1249CS1110ragot&@192.168.1.23:554//h264Preview_01_main" -rtsp_transport tcp image%03d.jpg
